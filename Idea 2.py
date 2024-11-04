7a8cbf1ac3b8946454a6417db4e5ba592bd91904
Jagrat <jagrat.sharma@torontomu.ca>
2024-11-04 11:39:28 -0500

    jagrat.sharma@torontomu.ca

---
+++ Idea 2.py
@@ -0,0 +1,11 @@
+"""
+This is the second idea for the cps 109 project
+
+The problem is that i am playing football manager using a small team which doesn't have a good scouting system and want
+to find good players but lack the budget and resources then stores the players you wnnt as a jsom file which ascts as
+shortlist this method is based on the moneyball strategy from the movie by the same name.
+"""
+
+"""
+What this program does is that 
+"""
