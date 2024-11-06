369585a6dad17d936c1c40f70281efabec6fcf82
Jagrat <jagrat.sharma@torontomu.ca>
2024-11-06 11:10:47 -0500

    jagrat.sharma@torontomu.ca

--- Idea 2.py
+++ Idea 2.py
@@ -1,11 +1,13 @@
 """
 This is the second idea for the cps 109 project
 
-The problem is that i am playing football manager using a small team which doesn't have a good scouting system and want
-to find good players but lack the budget and resources then stores the players you wnnt as a jsom file which ascts as
-shortlist this method is based on the moneyball strategy from the movie by the same name.
+The problem is that I am playing Football Manager using a small team which doesn't have a good scouting system and want
+to find good players but lack the budget and resources. I need a system which looks at my team and my budget to find
+better players than what i have on my team who are either comparatively cheap or on a free transfer while taking into
+account the global reputation of my team.
 """
 
 """
-What this program does is that uses a 
+To solve the above problem what i created is a python program which asks for the name of your team then asks for which
+ position 
 """
