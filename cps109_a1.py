bb54b4a6a9b1cfe394de799aedff805c52a853f3
Jagrat <jagrat.sharma@torontomu.ca>
2024-11-23 20:15:24 -0500

    jagrat.sharma@torontomu.ca

--- cps109_a1.py
+++ cps109_a1.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import datetime as dt
+import random as rd
+import os
 # ---------------------------------------
 # Problem Description
 # ---------------------------------------
@@ -12,7 +16,36 @@ scouts, they won't be able to find a replacement in time. So you need to find a 
 injured defender and is currently in form.
 
 So what this program does is it takes an .xlsx file containing the information of players from your Football Manager
-save and based on the stats of your injured defender it finds a good replacement.
+save and based on the stats of your injured defender it finds a good replacement while taking into account their age
+ and writes them in a json file which acts as your shortlist.
 
 This approach is inspired by the movie 'Moneyball' and uses a similar approach to find replacement players.
+
+This project uses three libraries: pandas, random and datetime
 """
+# ---------------------------------------
+# Code
+# ---------------------------------------
+def player_search():
+    pass
+
+
+def view_shortlist():
+    if os.path.exists(file):
+        pass
+    else:
+        pass
+
+
+print("\n\nWelcome To Football Manager Scouting System\n\nWould You Like To:\n1. Search for a player" )
+print("2. View Your Shortlist\n3. Exit this program")
+while True:
+    usr = input("Enter Your Choice: ")
+    if usr == "1":
+        player_search()
+    elif usr == "2":
+        view_shortlist()
+    elif usr == "3":
+        exit()
+    else:
+        print("Please input a valid choice")
